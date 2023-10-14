4682f1ea7039bf3c6d9bf70e3527b8852fec1900
sudhanshu8352 <82777068+sudhanshu8352@users.noreply.github.com>
2023-10-15 02:12:25 +0530

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,48 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <!-- <div
+            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
+        >
+            <a>
+                <i class="fa-brands fa-linkedin-in" />
+            </a>
+            <a>
+                <i class="fa-brands fa-github" />
+            </a>
+        </div>
+        <p class="mx-auto text-sm opacity-70">or</p>
+        <p class="">Hugh_jass@gmail.com</p> -->
+        <p><b class="pr-2">Email</b> shekharsudhanshu8352@gmail.com</p>
+        <p>
+            <i class="fa-brands fa-github" style="color: #ffffff;"></i>
+            <!-- <b class="pr-2">GitHub</b> -->
+            <a href="https://github.com/shekhar8352" target="_blank" class="text-violet-400"
+                >GitHub<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+        <p>
+            <i class="fa-brands fa-linkedin" style="color: #ffffff;"></i>
+            <!-- <b class="pr-2">LinkedIn</b> -->
+            <a href="https://www.linkedin.com/in/sudhanshu-shekhar-631683204/" target="_blank" class="text-violet-400"
+                >LinkedIn<sup class=""
+                    ><span class="text-xs scale-75 pl-0.5"
+                        ><i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        /></span
+                    ></sup
+                ></a
+            >
+        </p>
+    </div>
+</footer>
